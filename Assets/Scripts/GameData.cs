1fd01573458f60fea32ba0a6085f1556499f3cb9
LeopoldLecl <115979417+LeopoldLecl@users.noreply.github.com>
2023-05-31 17:30:46 +0200

    [ADD] Save System
    should not be definitive
    needs to turn it into ScObject

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+    public int levelProgression;
+
+    public bool[] levelUnlocked;
+    public bool[] spellUnlocked;
+
+    public int difficulty;
+    public int soundVolume;
+
+
+    public GameData(){
+        levelProgression = 0;
+        levelUnlocked = new bool[4];
+        levelUnlocked[0] = true;
+        spellUnlocked = new bool[13];
+        spellUnlocked[0] = true;
+        spellUnlocked[1] = true;
+        spellUnlocked[2] = true;
+        spellUnlocked[3] = true;
+        difficulty = 0;
+        soundVolume = 100;
+    }
+    
+    
+}
