1b32f01fb606eea332e9278449c53becd6eb8d00
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2023-05-31 10:26:38 +0200

    [Update] Debug Screen

--- Assets/Scripts/DebugCard.cs
+++ Assets/Scripts/DebugCard.cs
@@ -7,24 +7,29 @@ using UnityEngine.UI;
 public class DebugCard : MonoBehaviour
 {
     [SerializeField] CardBase[] cards;
-    [SerializeField] GameObject inputFieldPrefab;
     [SerializeField] GameObject template;
     int index = 0;
 
     public void ShowDebug()
     {
-        template.GetComponent<SetInputField>().Init(cards[0]);
-        template.transform.SetParent(transform);
-        template.transform.localPosition = Vector3.zero;
+        template.GetComponent<SetInputField>().Init(cards[index]);
     }
 
     public void Right()
     {
-
+        index++;
+        if(index >= cards.Length - 1)
+        {
+            index = cards.Length - 1;
+        }
     }
 
     public void Left()
     {
-
+        index--;
+        if (index < 0)
+        {
+            index = 0;
+        }
     }
 }
