af4e2b9d5307cae7b5084eb8a00e34ce981dfd31
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2023-06-01 10:50:46 +0200

    Update CardBase.cs

--- Assets/Scripts/CardSystems/CardBase.cs
+++ Assets/Scripts/CardSystems/CardBase.cs
@@ -1,15 +1,14 @@
+using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
-[CreateAssetMenu(fileName = "CardBase", menuName = "ScriptableObjects/CardBase")]
 public class CardBase : ScriptableObject
 {
-    public CardBehaviour cardBehaviour;
+    CardBehaviour cardBehaviour;
     public bool isEnableInGame = true;
     public string cardName;
     public int manaCost;
+
     public ApplyTo applyTo;
 
     bool doHeal
@@ -22,41 +21,66 @@ public class CardBase : ScriptableObject
 
     bool isTurnDependant
     {
-        heal,
-        unstableHeal,
-        unNaturalRegeneration,
-        massHeal,
-        regeneration,
-        vampirism,
-        godBet,
-        spiritGuard,
-        spiritShield,
-        spiritBarrier,
-        blessingOfMars,
-        blessingOfJupiter,
-        focusBoost,
-        allure,
-        trap,
-        speedBoost,
-        fortifyMana
+        get
+        {
+            return cardBehaviour == CardBehaviour.regeneration || cardBehaviour == CardBehaviour.resonanceShield || cardBehaviour == CardBehaviour.blessingOfMars || cardBehaviour == CardBehaviour.blessingOfJupiter || cardBehaviour == CardBehaviour.manaBoost || cardBehaviour == CardBehaviour.speedBoost;
+        }
     }
+
+    [ShowIf("doHeal")]
+    public int healthHealed;
+
+    [ShowIf("isTurnDependant")]
+    public int turnActive;
+
+    [ShowIf("cardBehaviour", CardBehaviour.resurection)]
+    public bool revive;
+
+    [ShowIf("cardBehaviour", CardBehaviour.manaBoost)]
+    public bool addedMana;
+
+    [ShowIf("cardBehaviour", CardBehaviour.unNaturalRegeneration)]
+    public float poisonChance;
+
+    [ShowIf("cardBehaviour", CardBehaviour.spiritShield)]
+    public int shieldBreakAfter;
+
+    [ShowIf("cardBehaviour", CardBehaviour.blessingOfMars)]
+    public int damageMultiplier;
+
+    [ShowIf("cardBehaviour", CardBehaviour.blessingOfJupiter)]
+    public int damageAdded;
 
     public void ApplyEffectOfTheCard(IHealable partyMember)
     {
         switch (cardBehaviour)
         {
-            case CardBehaviour.heal:
-                Debug.Log("InSwitch");
-                partyMember.Heal(2);
-                
-                break;
 
-            case CardBehaviour.unstableHeal:
-                Debug.Log("la carte est  unstableheal");
-                break;
+
         }
-    }
-    
+    }    
 }
 
+public enum CardBehaviour
+{
+    heal,
+    unNaturalRegeneration,
+    massHeal,
+    regeneration,
+    resurection,
+    antidote,
+    spiritShield,
+    spiritBarrier,
+    resonanceShield,
+    blessingOfMars,
+    blessingOfJupiter,
+    manaBoost,
+    manaRestauration,
+    speedBoost
+}
 
+public enum ApplyTo
+{
+    All,
+    Target
+}
