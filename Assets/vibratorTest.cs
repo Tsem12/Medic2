13eca45b95df38cdb79b03bab313b38a7aa53474
LeopoldLecl <115979417+LeopoldLecl@users.noreply.github.com>
2023-06-02 15:51:49 +0200

    [Add] vibration system
    non fuctionable yet

---
+++ Assets/vibratorTest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vibratorTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        VibrationScript.Vibrate(1000);   
+    }
+
+}
