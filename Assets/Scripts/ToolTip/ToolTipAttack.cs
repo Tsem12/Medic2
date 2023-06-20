280ba3602922f1afe384248cc40aed784d0548a6
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2023-06-20 14:45:51 +0200

    [ADD] ToolTip Card Selector New Version

---
+++ Assets/Scripts/ToolTip/ToolTipAttack.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolTipAttack : MonoBehaviour, IToolTip
+{
+    [SerializeField] Character character;
+
+    public void ShowToolTip(ToolTip tooltip)
+    {
+        character.
+    }
+}
